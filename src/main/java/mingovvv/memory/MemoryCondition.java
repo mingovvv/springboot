35d7722dec6cce55d8aedcc277d8fd8d81ea31eb
puregyu <puregyu@naver.com>
2023-10-22 02:52:15 +0900

    Feat : "@Conditional을 통한 빈 생성 옵션화"

---
+++ src/main/java/mingovvv/memory/MemoryCondition.java
@@ -0,0 +1,20 @@
+package mingovvv.memory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+// Condition 인터페이스 구현
+@Slf4j
+public class MemoryCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        // -Dmemory=on
+        String memory = context.getEnvironment().getProperty("memory");
+        log.info("memory={}", memory);
+        return "on".equals(memory);
+    }
+
+}
--- src/main/java/mingovvv/springboot/auto_config/config/MemoryConfig.java
+++ src/main/java/mingovvv/springboot/auto_config/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package mingovvv.springboot.auto_config.config;
 
+import mingovvv.memory.MemoryCondition;
 import mingovvv.memory.MemoryController;
 import mingovvv.memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) // MemoryCondition 클래스의 빈 생성조건을 참조
 public class MemoryConfig {
 
     @Bean
