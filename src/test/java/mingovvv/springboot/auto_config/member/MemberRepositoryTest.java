56f6726f347ee9b2378168da6c6332afca05e6f2
puregyu <puregyu@naver.com>
2023-10-22 01:22:18 +0900

    Feat: init

---
+++ src/test/java/mingovvv/springboot/auto_config/member/MemberRepositoryTest.java
@@ -0,0 +1,28 @@
+package mingovvv.springboot.auto_config.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void memberTest() {
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getMemberId());
+        assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+
+    }
+
+}
