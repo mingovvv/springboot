702aa6ee29dd1ea9bd35b2e4093d4bc99747c5ce
puregyu <puregyu@naver.com>
2023-10-22 04:01:42 +0900

    Feat : "직접 만든 외부라이브러리 적용"

---
+++ src/main/java/mingovvv/springboot/auto_config/config/ExMemoryConfig.java
@@ -0,0 +1,21 @@
+package mingovvv.springboot.auto_config.config;
+
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExMemoryConfig {
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+}
